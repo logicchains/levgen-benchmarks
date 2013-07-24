0321adff95b271903a8d0682575bc253be20c1d5
markhammons <markehammons@gmail.com>
2013-07-25 01:14:22 +0200

    Added Xorshift32 randomization along with some code cleanup.

--- scala/Main.scala
+++ scala/Main.scala
@@ -1,26 +1,35 @@
-import scala.util.Random
-
 case class Tile(X: Int, Y: Int, var T: Int)
 
-case class Room(X: Int, Y: Int, W: Int, H: Int, N: Int)
+case class Room(X: Int, Y: Int, W: Int, H: Int)
 
 case class CD(X: Int, Y: Int, L: Int, vert: Int)
 
 case class Lev(rs: Array[Room], ts: Array[Tile])
+
+case class Xorshift32(var seed: Int) {
+  def nextInt = {
+    seed ^= seed << 13
+    seed ^= seed >>> 17
+    seed ^= seed << 5
+    seed
+  }
+}
 
 object Main {
   val TileDim = 50
   val MinWid = 2
   val MaxWid = 8
 
+  val rand = Xorshift32(System.currentTimeMillis().toInt)
+
   def main(args: Array[String]) {
     println(s"The bench took ${time {
 
-      val v = args(0).toLong
+      val v = args(0).toInt
       println(s"The random seed is: $v")
-      Random.setSeed(v)
+      rand.seed = v
 
-      val ls = for(l <- 0 until 1000) yield {
+      val ls = for(l <- 0 until 100) yield {
         val rs = rooms(Stream.empty).take(99).toArray
         val ts = new Array[Tile](2500)
         for(i <- 0 until 2500) {
@@ -47,32 +56,29 @@ object Main {
 
 
   def makeRoom(rooms: Stream[Room]): Room = { //recursive, so result type must be specified
-    val x = math.abs(Random.nextInt%TileDim)
-    val y = math.abs(Random.nextInt%TileDim)
-    val w = math.abs(Random.nextInt%MaxWid+MinWid)
-    val h = math.abs(Random.nextInt%MaxWid+MinWid)
+    val x: Int = math.abs(rand.nextInt%TileDim)
+    val y = math.abs(rand.nextInt%TileDim)
+    val w = math.abs(rand.nextInt%MaxWid+MinWid)
+    val h = math.abs(rand.nextInt%MaxWid+MinWid)
     if(x+w>=TileDim || y+h>=TileDim || x==0 || y==0)
       makeRoom(rooms)
     else if(!checkColl(x,y,w,h,rooms)) {
-        Room(x,y,w,h,rooms.length)
+      Room(x,y,w,h)
     }
-    else makeRoom(rooms)
+    else
+      makeRoom(rooms)
   }
 
   def checkColl(x: Int,y: Int,w: Int,h: Int, rs: Stream[Room]): Boolean = {
-    for (room <- rs) {
-      if(!(room.X + room.W + 1 < x || room.X > x+w+1 || room.Y + room.H + 1 < y || room.Y > y+h+1))
+    for (oRoom <- rs) {
+      if(!(oRoom.X + oRoom.W + 1 < x || oRoom.X > x+w+1 || oRoom.Y + oRoom.H + 1 < y || oRoom.Y > y+h+1))
         return true
     }
     false
   }
 
   def room2Tiles(r: Room, ts: Array[Tile]) = {
-    val x=r.X
-    val y=r.Y
-    val w=r.W
-    val h=r.H
-    for (xi <- x until x+w+1; yi <- y until y+h+1) {
+    for (xi <- r.X until r.X+r.W+1; yi <- r.Y until r.Y+r.H+1) {
       val num = yi*TileDim+xi
       ts(num).T=1
     }
